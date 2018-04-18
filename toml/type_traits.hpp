9247280a79c0e1257e741b98d3e836513f02ba50
ToruNiina <niina.toru.68u@gmail.com>
2018-04-18 19:34:52 +0900

    add is_string_view_like to resolve overload correctly

--- toml/type_traits.hpp
+++ toml/type_traits.hpp
@@ -5,16 +5,25 @@
 #include <boost/mpl/and.hpp>
 #include <boost/mpl/or.hpp>
 #include <boost/mpl/not.hpp>
+#include <boost/tti/has_type.hpp>
 #include <boost/core/enable_if.hpp>
-#include <boost/cstdint.hpp>
 #include <boost/type_traits.hpp>
 #include <boost/iterator/iterator_traits.hpp>
-#include <boost/tti/has_type.hpp>
+#include <boost/cstdint.hpp>
 #include <boost/array.hpp>
+#include <boost/utility/string_ref.hpp>
+#include <boost/utility/string_view.hpp>
+#ifdef __has_include
+#  if __has_include(<string_view>)
+#    include <string_view>
+#    define TOML_HAS_CXX17_STRING_VIEW
+#  endif // string view
+#endif // has_include
 
 // if defined, include will be skipped.
 #ifndef BOOST_NO_CXX11_HDR_ARRAY
 #include <array>
+#define TOML_HAS_CXX11_ARRAY
 #endif // BOOST_NO_CXX11_HDR_ARRAY
 
 namespace toml
@@ -91,12 +100,17 @@ template<typename T> struct is_convertible<T, value>
 template<typename T> struct is_fixed_size_array : boost::false_type{};
 template<typename T, std::size_t N>
 struct is_fixed_size_array<boost::array<T, N> > : boost::true_type{};
-
-#ifndef BOOST_NO_CXX11_HDR_ARRAY
+#ifdef TOML_HAS_CXX11_ARRAY
 template<typename T, std::size_t N>
 struct is_fixed_size_array<std::array<T, N> > : boost::true_type{};
 #endif
 
+template<typename T> struct is_string_view_like : boost::false_type{};
+template<> struct is_string_view_like<boost::string_view> : boost::true_type{};
+template<> struct is_string_view_like<boost::string_ref>  : boost::true_type{};
+#ifdef TOML_HAS_CXX17_STRING_VIEW
+template<> struct is_string_view_like<std::string_view>   : boost::true_type{};
+#endif
 
 BOOST_TTI_HAS_TYPE(iterator)    // has_type_iterator
 BOOST_TTI_HAS_TYPE(value_type)  // has_type_value_type
@@ -104,20 +118,21 @@ BOOST_TTI_HAS_TYPE(key_type)    // has_type_key_type
 BOOST_TTI_HAS_TYPE(mapped_type) // has_type_mapped_type
 
 template<typename T> struct is_array_like : boost::mpl::and_<
-        has_type_iterator<T>,
-        has_type_value_type<T>,
-        boost::mpl::not_<has_type_key_type<T> >,
-        boost::mpl::not_<has_type_mapped_type<T> >,
-        boost::mpl::not_<is_fixed_size_array<T> >
+        boost::mpl::not_<is_toml_type<T> >,        // is not an exact toml type
+        boost::mpl::not_<is_string_view_like<T> >, // is not a string_view type
+        boost::mpl::not_<is_fixed_size_array<T> >, // is not an array type
+        has_type_iterator<T>,                      // has iterator
+        has_type_value_type<T>,                    // has value_type
+        boost::mpl::not_<has_type_key_type<T> >,   // does not have key_type
+        boost::mpl::not_<has_type_mapped_type<T> > // does not have mapped_type
     >{};
 template<typename T> struct is_map_like : boost::mpl::and_<
-        has_type_iterator<T>,
-        has_type_value_type<T>,
-        has_type_key_type<T>,
-        has_type_mapped_type<T>
+        boost::mpl::not_<is_toml_type<T> >,        // is not an exact toml type
+        has_type_iterator<T>,                      // has iterator
+        has_type_value_type<T>,                    // has value_type
+        has_type_key_type<T>,                      // has key_type
+        has_type_mapped_type<T>                    // has mapped_type
     >{};
-
-
 
 } // toml
 #endif// TOML_TYPE_TRAITS_HPP
