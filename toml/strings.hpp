1a9a976b42a6ec40eeadb92470987c8f8f5c6499
ToruNiina <niina.toru.68u@gmail.com>
2018-04-30 19:15:29 +0900

    remove tag type

--- toml/strings.hpp
+++ toml/strings.hpp
@@ -8,11 +8,6 @@
 namespace toml
 {
 
-struct basic_tag{};
-struct literal_tag{};
-extern const basic_tag   basic_string;
-extern const literal_tag literal_string;
-
 struct string
 {
     enum kind_t
@@ -27,26 +22,18 @@ struct string
     string& operator=(const string& s)
     {kind = s.kind; str = s.str; return *this;}
 
-    string(const std::string& s): kind(basic), str(s){}
-    string(const char* s)       : kind(basic), str(s){}
-
-    string(const std::string& s, basic_tag)  : kind(basic),   str(s){}
-    string(const char* s,        basic_tag)  : kind(basic),   str(s){}
-    string(const std::string& s, literal_tag): kind(literal), str(s){}
-    string(const char* s,        literal_tag): kind(literal), str(s){}
-    string(const std::string& s, kind_t k)   : kind(k),       str(s){}
-    string(const char* s,        kind_t k)   : kind(k),       str(s){}
+    string(const std::string& s)          : kind(basic), str(s){}
+    string(const std::string& s, kind_t k): kind(k),     str(s){}
+    string(const char* s)                 : kind(basic), str(s){}
+    string(const char* s,        kind_t k): kind(k),     str(s){}
 
     string& operator=(const std::string& s)
     {kind = basic; str = s; return *this;}
 
 #ifdef BOOST_HAS_RVALUE_REFS
-    string(string&& s) : kind(s.kind), str(std::move(s.str)){}
-
-    string(std::string&& s) : kind(basic), str(std::move(s)){}
-    string(std::string&& s, basic_tag)  : kind(basic),   str(std::move(s)){}
-    string(std::string&& s, literal_tag): kind(literal), str(std::move(s)){}
-    string(std::string&& s, kind_t k)   : kind(k),       str(std::move(s)){}
+    string(string&& s)               : kind(s.kind), str(std::move(s.str)){}
+    string(std::string&& s)          : kind(basic),  str(std::move(s)){}
+    string(std::string&& s, kind_t k): kind(k),      str(std::move(s)){}
 
     string& operator=(string&& s)
     {kind = s.kind; str = std::move(s.str); return *this;}
@@ -119,7 +106,6 @@ operator<<(std::basic_ostream<charT, traits>& os, const string& str)
     os << str.str;
     return os;
 }
-
 
 } // toml
 #endif// TOML_STRINGS_HPP
