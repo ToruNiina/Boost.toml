417599614f379b74d71ee536265a2527bab7f3dc
ToruNiina <niina.toru.68u@gmail.com>
2018-05-14 14:10:13 +0900

    add copyright notice

--- toml/lexer.hpp
+++ toml/lexer.hpp
@@ -1,3 +1,7 @@
+//              Copyright Toru Niina 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
 #ifndef TOML_LEXER_HPP
 #define TOML_LEXER_HPP
 #include <toml/combinator.hpp>
