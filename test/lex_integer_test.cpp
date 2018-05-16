c47304d9dd49ef9c79e845664c166d58ae9682cc
ToruNiina <niina.toru.68u@gmail.com>
2018-05-16 10:49:35 +0900

    update lex_integer_test

--- test/lex_integer_test.cpp
+++ test/lex_integer_test.cpp
@@ -1,116 +1,103 @@
 #define BOOST_TEST_MODULE "parse_integer_test"
-#include <toml/parser.hpp>
+#include <toml/lexer.hpp>
 #include <boost/test/included/unit_test.hpp>
 #include <iostream>
 #include <iomanip>
+#include "lex_aux.hpp"
 
-inline void check_equal(const std::string token, const toml::integer answer)
-{
-    const toml::detail::result<toml::integer, std::string::const_iterator>
-        result = toml::detail::parse_integer(token.begin(), token.end());
-    BOOST_CHECK(result);
-    BOOST_CHECK(result.iterator() == token.end());
-    if(!result)
-    {
-        std::cerr << result << std::endl;
-    }
-    BOOST_CHECK_EQUAL(result.unwrap(), answer);
-}
-inline void check_fail(const std::string token)
-{
-    const toml::detail::result<toml::integer, std::string::const_iterator>
-        result = toml::detail::parse_integer(token.begin(), token.end());
-    BOOST_CHECK(!result);
-    if(result)
-    {
-        std::cerr << result << std::endl;
-    }
-    std::cout << "successfully failed. error message is -> "
-              << result.unwrap_err() << std::endl;
-}
-inline void check_partially_read(const std::string token)
-{
-    const toml::detail::result<toml::integer, std::string::const_iterator>
-        result = toml::detail::parse_integer(token.begin(), token.end());
-    BOOST_CHECK(result.iterator() != token.begin());
-    BOOST_CHECK(result.iterator() != token.end());
-}
+using namespace toml;
+using namespace detail;
 
 BOOST_AUTO_TEST_CASE(test_decimal_correct)
 {
-    check_equal("1234",             1234);
-    check_equal("+1234",           +1234);
-    check_equal("-1234",           -1234);
-    check_equal("0",                   0);
-    check_equal("1_2_3_4",          1234);
-    check_equal("+1_2_3_4",         1234);
-    check_equal("-1_2_3_4",        -1234);
-    check_equal("123_456_789", 123456789);
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "1234",        "1234"       );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "+1234",       "+1234"      );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "-1234",       "-1234"      );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0",           "0"          );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "1_2_3_4",     "1_2_3_4"    );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "+1_2_3_4",    "+1_2_3_4"   );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "-1_2_3_4",    "-1_2_3_4"   );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "123_456_789", "123_456_789");
 }
 
 BOOST_AUTO_TEST_CASE(test_decimal_invalid)
 {
-    check_fail("01234");
-    check_fail("_1234");
-    check_fail("123__45");
-    check_partially_read("123+45");
-    check_partially_read("123-45");
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "123+45",  "123");
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "123-45",  "123");
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "01234",   "0");
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "123__45", "123");
+
+    TOML_LEX_CHECK_REJECT(lex_integer, "_1234");
 }
 
 BOOST_AUTO_TEST_CASE(test_hex_correct)
 {
-    check_equal("0xDEADBEEF",  3735928559);
-    check_equal("0xdeadbeef",  3735928559);
-    check_equal("0xDEADbeef",  3735928559);
-    check_equal("0xDEAD_BEEF", 3735928559);
-    check_equal("0xdead_beef", 3735928559);
-    check_equal("0xdead_BEEF", 3735928559);
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0xDEADBEEF",  "0xDEADBEEF" );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0xdeadbeef",  "0xdeadbeef" );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0xDEADbeef",  "0xDEADbeef" );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0xDEAD_BEEF", "0xDEAD_BEEF");
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0xdead_beef", "0xdead_beef");
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0xdead_BEEF", "0xdead_BEEF");
 
-    check_equal("0xFF",     255);
-    check_equal("0x00FF",   255);
-    check_equal("0x0000FF", 255);
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0xFF",     "0xFF"    );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0x00FF",   "0x00FF"  );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0x0000FF", "0x0000FF");
 }
 
 BOOST_AUTO_TEST_CASE(test_hex_invalid)
 {
-    check_fail("0x_DEADBEEF");
-    check_fail("0xDEAD__BEEF");
-    check_fail("0x+DEADBEEF");
-    check_fail("-0xFF");
-    check_fail("-0x00FF");
-    check_partially_read("0xAPPLE");
-    check_partially_read("0xDEAD+BEEF");
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0xAPPLE",     "0xA");
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0xDEAD+BEEF", "0xDEAD");
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0xDEAD__BEEF", "0xDEAD");
+
+    TOML_LEX_CHECK_REJECT(lex_hex_int, "0x_DEADBEEF");
+    TOML_LEX_CHECK_REJECT(lex_hex_int, "0x+DEADBEEF");
+    TOML_LEX_CHECK_REJECT(lex_hex_int, "-0xFF"      );
+    TOML_LEX_CHECK_REJECT(lex_hex_int, "-0x00FF"    );
+
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0x_DEADBEEF", "0" );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0x+DEADBEEF", "0" );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "-0xFF"      , "-0" );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "-0x00FF"    , "-0" );
 }
 
 BOOST_AUTO_TEST_CASE(test_oct_correct)
 {
-    check_equal("0o777",   511);
-    check_equal("0o7_7_7", 511);
-    check_equal("0o007",     7);
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0o777",    "0o777"  );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0o7_7_7",  "0o7_7_7");
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0o007",    "0o007"  );
+
 }
 
 BOOST_AUTO_TEST_CASE(test_oct_invalid)
 {
-    check_fail("0o800");
-    check_fail("-0o777");
-    check_fail("0o+777");
-    check_fail("0o1__0");
-    check_fail("0o_10");
-    check_partially_read("0o77+7");
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0o77+7", "0o77");
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0o1__0", "0o1");
+
+    TOML_LEX_CHECK_REJECT(lex_oct_int, "0o800" );
+    TOML_LEX_CHECK_REJECT(lex_oct_int, "-0o777");
+    TOML_LEX_CHECK_REJECT(lex_oct_int, "0o+777");
+    TOML_LEX_CHECK_REJECT(lex_oct_int, "0o_10" );
+
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0o800",  "0");
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "-0o777", "-0");
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0o+777", "0");
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0o_10",  "0");
 }
 
 BOOST_AUTO_TEST_CASE(test_bin_correct)
 {
-    check_equal("0b10000",    16);
-    check_equal("0b010000",   16);
-    check_equal("0b01_00_00", 16);
-    check_equal("0b111111",   63);
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0b10000",    "0b10000"   );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0b010000",   "0b010000"  );
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0b01_00_00", "0b01_00_00");
+    TOML_LEX_CHECK_ACCEPT(lex_integer, "0b111111",   "0b111111"  );
 }
 
 BOOST_AUTO_TEST_CASE(test_bin_invalid)
 {
-    check_fail("-0b10000");
-    check_fail("0b11__11");
-    check_fail("0b_1111");
-    check_partially_read("0b11+11");
+    TOML_LEX_CHECK_ACCEPT(lex_bin_int, "0b11__11", "0b11");
+    TOML_LEX_CHECK_ACCEPT(lex_bin_int, "0b11+11" , "0b11");
+
+    TOML_LEX_CHECK_REJECT(lex_bin_int, "-0b10000");
+    TOML_LEX_CHECK_REJECT(lex_bin_int, "0b_1111" );
 }
