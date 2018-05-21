46397e1e4ad2c7f9c8fa827888d2592ab3d75626
ToruNiina <niina.toru.68u@gmail.com>
2018-05-21 19:04:55 +0900

    output only error message when failed

--- test/parse_aux.hpp
+++ test/parse_aux.hpp
@@ -3,7 +3,7 @@
     std::string::const_iterator iter = token.begin();\
     const result<tp, std::string> r = psr(iter, token.end());\
     BOOST_CHECK(r.is_ok());\
-    if(r){BOOST_CHECK_EQUAL(r.unwrap(), expct);}\
-    else{std::cerr << r << std::endl;}\
+    if(r){BOOST_CHECK(r.unwrap() == expct);}\
+    else{std::cerr << r.unwrap_err() << std::endl;}\
     BOOST_CHECK(iter == token.end());\
 } /**/
