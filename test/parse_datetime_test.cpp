36b116dd8cfee62c481deb9d9db6c7b727ae58d9
ToruNiina <niina.toru.68u@gmail.com>
2018-05-22 09:50:57 +0900

    add testcode of parse_value for datetime

--- test/parse_datetime_test.cpp
+++ test/parse_datetime_test.cpp
@@ -21,10 +21,27 @@ BOOST_AUTO_TEST_CASE(test_time)
         toml::time,  hours(7) + minutes(32) + milliseconds(999) + microseconds(999));
 }
 
+BOOST_AUTO_TEST_CASE(test_time_value)
+{
+    TOML_PARSE_CHECK_EQUAL(parse_value, "07:32:00",
+        value, value(hours(7) + minutes(32)));
+    TOML_PARSE_CHECK_EQUAL(parse_value, "07:32:00.99",
+        value, value(hours(7) + minutes(32) + milliseconds(990)));
+    TOML_PARSE_CHECK_EQUAL(parse_value, "07:32:00.999",
+        value, value(hours(7) + minutes(32) + milliseconds(999)));
+    TOML_PARSE_CHECK_EQUAL(parse_value, "07:32:00.999999",
+        value, value(hours(7) + minutes(32) + milliseconds(999) + microseconds(999)));
+}
+
 BOOST_AUTO_TEST_CASE(test_date)
 {
     TOML_PARSE_CHECK_EQUAL(parse_local_date, "1979-05-27",
         toml::date, toml::date(1979, 5, 27));
+}
+BOOST_AUTO_TEST_CASE(test_date_value)
+{
+    TOML_PARSE_CHECK_EQUAL(parse_value, "1979-05-27",
+        value, value(toml::date(1979, 5, 27)));
 }
 
 BOOST_AUTO_TEST_CASE(test_datetime)
@@ -48,6 +65,27 @@ BOOST_AUTO_TEST_CASE(test_datetime)
     TOML_PARSE_CHECK_EQUAL(parse_local_datetime, "1979-05-27 07:32:00.999999",
         toml::local_datetime, toml::local_datetime(toml::date(1979, 5, 27),
             toml::hours(7) + toml::minutes(32) + toml::milliseconds(999) + toml::microseconds(999)));
+}
+
+BOOST_AUTO_TEST_CASE(test_datetime_value)
+{
+    TOML_PARSE_CHECK_EQUAL(parse_value, "1979-05-27T07:32:00",
+        value, value(toml::date(1979, 5, 27), toml::hours(7) + toml::minutes(32)));
+    TOML_PARSE_CHECK_EQUAL(parse_value, "1979-05-27T07:32:00.99",
+        value, value(toml::date(1979, 5, 27), toml::hours(7) + toml::minutes(32) +
+            toml::milliseconds(990)));
+    TOML_PARSE_CHECK_EQUAL(parse_value, "1979-05-27T07:32:00.999999",
+        value, value(toml::date(1979, 5, 27), toml::hours(7) + toml::minutes(32) +
+            toml::milliseconds(999) + toml::microseconds(999)));
+
+    TOML_PARSE_CHECK_EQUAL(parse_value, "1979-05-27 07:32:00",
+        value, value(toml::date(1979, 5, 27), toml::hours(7) + toml::minutes(32)));
+    TOML_PARSE_CHECK_EQUAL(parse_value, "1979-05-27 07:32:00.99",
+        value, value(toml::date(1979, 5, 27), toml::hours(7) + toml::minutes(32) +
+            toml::milliseconds(990)));
+    TOML_PARSE_CHECK_EQUAL(parse_value, "1979-05-27 07:32:00.999999",
+        value, value(toml::date(1979, 5, 27), toml::hours(7) + toml::minutes(32) +
+            toml::milliseconds(999) + toml::microseconds(999)));
 }
 
 BOOST_AUTO_TEST_CASE(test_offset_datetime)
@@ -76,5 +114,34 @@ BOOST_AUTO_TEST_CASE(test_offset_datetime)
 
         BOOST_CHECK(r.is_ok());
         BOOST_CHECK(r.unwrap().utc_time() == answer);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(test_offset_datetime_value)
+{
+    TOML_PARSE_CHECK_EQUAL(parse_value, "1979-05-27T07:32:00Z",
+        value, value(toml::local_datetime(
+            toml::date(1979, 5, 27), toml::hours(7) + toml::minutes(32)),
+            time_zone_ptr(new boost::local_time::posix_time_zone("UTC"))));
+    TOML_PARSE_CHECK_EQUAL(parse_value, "1979-05-27T07:32:00.99Z",
+        value, value(toml::local_datetime(toml::date(1979, 5, 27),
+            toml::hours(7) + toml::minutes(32) + toml::milliseconds(990)),
+            time_zone_ptr(new boost::local_time::posix_time_zone("UTC"))));
+    TOML_PARSE_CHECK_EQUAL(parse_value, "1979-05-27T07:32:00.999999Z",
+        value, value(toml::local_datetime(
+            toml::date(1979, 5, 27), toml::hours(7) + toml::minutes(32) +
+            toml::milliseconds(999) + toml::microseconds(999)),
+            time_zone_ptr(new boost::local_time::posix_time_zone("UTC"))));
+
+    {
+        const std::string token("1979-05-27T18:50:00-04:00");
+        std::string::const_iterator iter(token.begin());
+        const result<toml::value, std::string> r =
+            parse_value(iter, token.end());
+        const toml::local_datetime answer(
+            toml::date(1979, 5, 27), toml::hours(22) + toml::minutes(50));
+
+        BOOST_CHECK(r.is_ok());
+        BOOST_CHECK(r.unwrap().get<toml::offset_datetime>().utc_time() == answer);
     }
 }
