f37214f696756c662dbe918901a1f13b6fdf5007
ToruNiina <niina.toru.68u@gmail.com>
2018-05-30 17:42:22 +0900

    add all-in-one header file

---
+++ toml/toml.hpp
@@ -0,0 +1,13 @@
+//              Copyright Toru Niina 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+#ifndef TOML_TOML_HPP
+#define TOML_TOML_HPP
+
+#include <toml/predefine.hpp>
+#include <toml/parser.hpp>
+#include <toml/serializer.hpp>
+#include <toml/get.hpp>
+
+#endif // TOML_TOML_HPP
